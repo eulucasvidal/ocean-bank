ada70c9613f3c65b2785efa59524ab70ced62f89
Lucas Vidal <luccas.lima12@gmail.com>
2024-01-31 09:59:35 -0300

    Criando sectios de perguntas frequentes

---
+++ js/modules/perguntas.js
@@ -0,0 +1,19 @@
+export default function initPerguntas(){
+  const perguntas = document.querySelectorAll(".perguntas button");
+
+  function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle('ativo');
+    const ativa = resposta.classList.contains("ativo")
+    pergunta.setAttribute("aria-expanded", "true");
+  }
+
+  function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta);
+  }
+
+  perguntas.forEach(eventosPerguntas);
+}
--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 import initToggleMenu from "./modules/menu-mobile.js";
 initToggleMenu();
+
+import initPerguntas from "./modules/perguntas.js";
+initPerguntas();
