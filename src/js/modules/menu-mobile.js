0f465840418c967ef27201fd3d807c67a908225c
Lucas Vidal <luccas.lima12@gmail.com>
2025-03-10 15:07:55 -0300

    Alterando cores e var de cores do projeto

--- src/js/modules/menu-mobile.js
+++ src/js/modules/menu-mobile.js
@@ -1,13 +1,16 @@
 export default function initToggleMenu() {
   const menuMobile = document.querySelector('[data-menu="suave"]');
   const botaoMobile = document.querySelector('[data-menu="button"]');
+  const botaoX = document.querySelector('[data-menu="button-x"]');
 
   function openMenu(event) {
     if (event.type === 'touchstart')event.preventDefault();
     menuMobile.classList.toggle("ativo");
     botaoMobile.classList.toggle("ativo");
+    botaoX.classList.toggle("ativo");
   }
-
+  botaoX.addEventListener("click",openMenu);
+  botaoX.addEventListener("touchstart",openMenu);
   botaoMobile.addEventListener("click", openMenu);
   botaoMobile.addEventListener("touchstart", openMenu);
 }
